6207d2da3b87700b63858d2df9de293b34d72035
irem ates <irematees16@gmail.com>
2024-03-18 13:13:29 +0300

    Add routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MemberView from '@/views/MemberView.vue'
+import MemberAlbumView from '@/views/MemberAlbumView.vue'
+import MemberPostsView from '@/views/MemberPostsView.vue'
+import MemberTodoView from '@/views/MemberTodoView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +14,24 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/member/:memberId',
+      name: 'member',
+      component: MemberView
+    },
+    {
+      path: '/member/:memberId/albums',
+      name: 'albums',
+      component: MemberAlbumView
+    },
+    {
+      path: '/member/:memberId/posts',
+      name: 'posts',
+      component: MemberPostsView
+    },
+    {
+      path: '/member/:memberId/todo',
+      name: 'todo',
+      component: MemberTodoView
     }
   ]
 })
